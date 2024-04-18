159f9b1bc42319bf2f9949b5bbebc88fce84c5db
rakiburrahman307 <rakiburrahman307@gmail.com>
2024-04-19 00:27:04 +0600

    payment getway added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 require('dotenv').config()
+const axios = require('axios')
+const { NagadGateway } = require('nagad-payment-gateway');
+const globals = require('node-global-storage')
+const { v4: uuidv4 } = require('uuid')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
+const SSLCommerzPayment = require('sslcommerz-lts')
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const jwt = require('jsonwebtoken');
 var cookieParser = require('cookie-parser')
@@ -9,6 +14,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
+
+const store_id = 'testi65faa225780a1'
+const store_passwd = 'testi65faa225780a1@ssl'
+const is_live = false
 // Middle Ware
 app.use(express.json());
 app.use(cors({
@@ -42,8 +51,40 @@ const verifyToken = (req, res, next) => {
 
     }
 };
+const config = {
+    apiVersion: 'development',
+    baseURL: 'http://sandbox.mynagad.com:10060/check-out/MDYyODAwNTcyNTYxNi42ODMwMDIwMDcxMDQyMjUuOU5PTEFVNkVaWkdUWVRBLmJiZGMyNTE3MTVmZTNiNjIzN2Zk',
+    callbackURL: 'https://example.com/payment/success/id=4',
+    merchantID: '6800000025',
+    merchantNumber: '016XXXXXXXX',
+    isPath: true,
+};
+const nagad = new NagadGateway(config);
 
+const bkashAuth = async (req, res, next) => {
 
+    globals.unset('id_token')
+
+    try {
+        const { data } = await axios.post(process?.env?.bkash_grant_token_url, {
+            app_key: process?.env?.bkash_api_key,
+            app_secret: process?.env.bkash_secret_key,
+        }, {
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                username: process?.env?.bkash_username,
+                password: process?.env?.bkash_password,
+            }
+        })
+
+        globals.set('id_token', data?.id_token, { protected: true })
+
+        next()
+    } catch (error) {
+        return res.status(401).send({ error: error?.message })
+    }
+}
 
 const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.044ysfk.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -409,8 +450,6 @@ async function run() {
         app.post('/create-payment-intent', async (req, res) => {
             const { price } = req.body;
             const amount = parseInt(price * 100);
-            console.log(amount, 'amount inside the intent')
-
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
                 currency: 'usd',
@@ -421,6 +460,252 @@ async function run() {
                 clientSecret: paymentIntent.client_secret
             })
         });
+        // --------------------------------------------------------------------------
+        // --------------------------------SSl Payment Gateway--------------------------------
+        // --------------------------------------------------------------------------
+
+        const tranId = new ObjectId().toString();
+        app.post('/payment/ssl', async (req, res) => {
+            const { info } = req?.body;
+            const product = await paymentType.findOne({ _id: new ObjectId(info?.id) })
+            const data = {
+                total_amount: product?.price,
+                currency: 'BDT',
+                tran_id: tranId, // use unique tran_id for each api call
+                success_url: `http://localhost:5000/payment/success/${tranId}`,
+                fail_url: `http://localhost:5000/payment/fail/${tranId}`,
+                cancel_url: 'http://localhost:5000/payment/cancel',
+                ipn_url: 'http://localhost:3030/ipn',
+                shipping_method: 'Courier',
+                product_name: product?.name,
+                product_category: 'web',
+                product_profile: 'general',
+                cus_name: info?.userName,
+                cus_email: info?.userEmail,
+                cus_add1: 'Dhaka',
+                cus_add2: 'Dhaka',
+                cus_city: 'Dhaka',
+                cus_state: 'Dhaka',
+                cus_postcode: '1000',
+                cus_country: 'Bangladesh',
+                cus_phone: '01711111111',
+                cus_fax: '01711111111',
+                ship_name: 'Customer Name',
+                ship_add1: 'Dhaka',
+                ship_add2: 'Dhaka',
+                ship_city: 'Dhaka',
+                ship_state: 'Dhaka',
+                ship_postcode: 1000,
+                ship_country: 'Bangladesh',
+            };
+
+            const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+            sslcz.init(data).then(apiResponse => {
+                // Redirect the user to payment gateway
+                let GatewayPageURL = apiResponse.GatewayPageURL
+                res.send({ url: GatewayPageURL })
+
+            });
+
+            const finalOrder = {
+                name: info?.userName,
+                email: info?.userEmail,
+                package: product?.name,
+                paymentType: 'sslcommerz',
+                Price: product?.price,
+                paidStatus: false,
+                transactionID: tranId
+            }
+            const result = await perchesPlanUsers.insertOne(finalOrder);
+
+
+            app.post('/payment/success/:tranId', async (req, res) => {
+                const id = req?.params?.tranId;
+                const result = await perchesPlanUsers.updateOne({ transactionID: id }, {
+                    $set: {
+                        paidStatus: true,
+                    }
+                });
+
+                if (result?.modifiedCount > 0) {
+                    res.redirect(`http://localhost:5173/payment/success/${id}`)
+                }
+            });
+            app.post('/payment/fail/:tranId', async (req, res) => {
+                const id = req?.params?.tranId;
+                const result = await perchesPlanUsers.deleteOne({ transactionID: id })
+                if (result?.deletedCount > 0) {
+                    res.redirect(`http://localhost:5173/payment/fail/${id}`)
+                }
+            });
+        })
+        // --------------------------------------------------------------------------
+        // --------------------------------SSL Payment Gateway--------------------------------
+        // --------------------------------------------------------------------------
+
+        // --------------------------------------------------------------------------
+        // --------------------------------Bkash Payment Gateway--------------------------------
+        // --------------------------------------------------------------------------
+
+        const bkash_headers = async () => {
+            return {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+                authorization: globals.get('id_token'),
+                'x-app-key': process.env.bkash_api_key,
+            }
+        }
+        app.post("/bkash-checkout", bkashAuth, async (req, res) => {
+            const { details } = req?.body;
+            globals.set('productId', details?.id)
+            globals.set('userName', details?.userName)
+            globals.set('userEmail', details?.userEmail)
+            const product = await paymentType.findOne({ _id: new ObjectId(details?.id) })
+            try {
+                const { data } = await axios.post(process.env.bkash_create_payment_url, {
+                    mode: '0011',
+                    payerReference: " ",
+                    callbackURL: `${process.env.callbackURL}/bkash/payment/callback`,
+                    amount: product?.price,
+                    currency: "BDT",
+                    intent: 'sale',
+                    merchantInvoiceNumber: 'Inv' + uuidv4().substring(0, 9)
+                }, {
+                    headers: await bkash_headers()
+                })
+                return res.status(200).send({ bkashURL: data?.bkashURL })
+            } catch (error) {
+                return res.status(401).send({ error: error?.message })
+            }
+
+
+        })
+
+        app.get("/bkash/payment/callback", bkashAuth, async (req, res) => {
+            const { paymentID, status } = req.query
+
+            if (status === 'cancel' || status === 'failure') {
+                return res.redirect(`http://localhost:5173/error?message=${status}`)
+            }
+            if (status === 'success') {
+                try {
+                    const { data } = await axios.post(process?.env?.bkash_execute_payment_url, { paymentID }, {
+                        headers: await bkash_headers()
+                    })
+                    const product = await paymentType.findOne({ _id: new ObjectId(globals.get('productId')) })
+
+                    if (data && data?.statusCode === '0000') {
+                        const finalOrder = {
+                            name: globals.get('userName'),
+                            email: globals.get('userEmail'),
+                            paymentType: 'Bkash',
+                            package: product?.name,
+                            Price: parseInt(data?.amount) || product?.price,
+                            customerMsisdn: data?.customerMsisdn,
+                            transactionStatus: data?.transactionStatus,
+                            paidStatus: true,
+                            trxID: data?.trxID,
+                            paymentID: data?.paymentID,
+                            merchantInvoiceNumber: data?.merchantInvoiceNumber,
+                            date: data?.paymentExecuteTime,
+                        }
+                        await perchesPlanUsers.insertOne(finalOrder);
+
+                        return res.redirect(`http://localhost:5173/payment/success/${"hi"}`)
+                    } else {
+                        return res.redirect(`http://localhost:5173/error?message=${data?.statusMessage}`)
+                    }
+                } catch (error) {
+                    console.log(error)
+                    return res.redirect(`http://localhost:5173/error?message=${error?.message}`)
+                }
+            }
+        })
+
+        // Add this route under admin middleware
+        app.post("/bkash-refund", bkashAuth, async (req, res) => {
+            const { trxID } = req.params;
+            try {
+                const payment = await perchesPlanUsers.findOne({ trxID })
+
+                const { data } = await axios.post(process.env.bkash_refund_transaction_url, {
+                    paymentID: payment?.paymentID,
+                    amount: payment?.amount,
+                    trxID,
+                    sku: 'payment',
+                    reason: 'cashback'
+                }, {
+                    headers: await bkash_headers()
+                })
+                if (data && data.statusCode === '0000') {
+                    return res.status(200).json({ message: 'refund success' })
+                } else {
+                    return res.status(404).json({ error: 'refund failed' })
+                }
+            } catch (error) {
+                return res.status(404).json({ error: 'refund failed' })
+            }
+        })
+
+        app.get("/bkash-search", async (req, res) => {
+            try {
+                const { trxID } = req.query
+                const result = await searchTransaction(bkashConfig, trxID)
+                res.send(result)
+            } catch (e) {
+                console.log(e)
+            }
+        })
+
+        app.get("/bkash-query", async (req, res) => {
+            try {
+                const { paymentID } = req.query
+                const result = await queryPayment(bkashConfig, paymentID)
+                res.send(result)
+            } catch (e) {
+                console.log(e)
+            }
+        })
+
+        // --------------------------------------------------------------------------
+        // --------------------------------Bkash Payment Gateway End Here--------------------------------
+        // --------------------------------------------------------------------------
+        // --------------------------------------------------------------------------
+        // --------------------------------Nagad Payment Gateway Start Here--------------------------------
+        // --------------------------------------------------------------------------
+      // Route to create a payment
+app.post('/nagad-checkout', async (req, res) => {
+    try {
+        // const nagadURL = await nagad.createPayment({
+        //     amount: '100',
+        //     ip: '10.10.0.10',
+        //     orderId: `${Date.now()}`,
+        //     productDetails: { a: '1', b: '2' },
+        //     clientType: 'PC_WEB',
+        // });
+        // Redirect user to the Nagad URL
+        res.send({ url: nagadURL }); 
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// Route to verify a payment
+app.post('/verify-payment/:paymentRefID', async (req, res) => {
+    const paymentRefID = req?.params?.paymentRefID;
+    try {
+        const nagadURL = await nagad.verifyPayment(paymentRefID);
+        // Redirect user to the Nagad URL
+        res.redirect(nagadURL);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+        // --------------------------------------------------------------------------
+        // --------------------------------Nagad Payment Gateway End Here--------------------------------
+        // --------------------------------------------------------------------------
 
         // request for the meals api 
         app.post('/req_meal', async (req, res) => {
@@ -458,7 +743,6 @@ async function run() {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) };
             const info = req.body;
-            console.log(info.status);
             const updateDoc = {
                 $set: {
                     status: info?.status
